40d62c25e8408ed514567a5ff1992acdd69cf842
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-02-14 09:58:31 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.tugalsan.api.file.pdf.pdfbox3.pdf2dom.fontverter {
     requires java.desktop;
     requires org.apache.commons.io;
-    requires guava.primitives.r03;
+    requires com.google.common;
     requires org.apache.commons.lang3;
     requires org.reflections;
     requires org.slf4j;
